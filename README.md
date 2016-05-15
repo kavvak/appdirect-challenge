a56d78dd115578df284d3a75d0a2939e379797bf
Kaveh Vakili <kaveh.vakili@gmail.com>
2016-05-15 17:46:02 -0400

    Initial Setup

---
+++ README.md
@@ -0,0 +1,13 @@
+# appdirect-code-challenge-solution
+
+## Setup
+
+Install the project dependencies:
+
+`npm install`
+
+## Running
+
+Starts the static and proxy servers:
+
+`npm start`
---
+++ server.js
@@ -0,0 +1,7 @@
+var exec = require('child_process').exec;
+
+exec('twitter-proxy');
+exec('http-server ./dist -a localhost -p 8000 -c-1');
+
+console.log('Server running on http://localhost:8000');
+console.log('Twitter API running on http://localhost:7890');
