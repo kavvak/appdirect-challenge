15b7cd404b8548531214e10e38d1f0386b99d318
Kaveh Vakili <kaveh.vakili@gmail.com>
2016-05-17 03:13:09 -0400

    removing server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-var exec = require('child_process').exec;
-
-exec('twitter-proxy');
-exec('http-server ./dist -a localhost -p 8000 -c-1');
-
-console.log('Server running on http://localhost:8000');
-console.log('twitter API running on http://localhost:7890');
